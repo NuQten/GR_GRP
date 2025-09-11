a25ac322d155feaba79c4a96c308cd2b9a2323f6
Quentin CONANEC <quentin.conanec@gmail.com>
2025-09-11 20:50:33 +0200

    Create getCSV.py & all_gr_data.csv

--- getCSV.py
+++ getCSV.py
@@ -1,6 +1,17 @@
-import csv
+import pandas
+import json
 import os
 
+ABSPATH_FILE = os.path.abspath(__file__)
+ABSPATH_PROJECT = os.path.dirname(ABSPATH_FILE)
 
 
-# Example usage
+if __name__ == "__main__":
+
+    # Convert JSON to DataFrame
+    data_frame = pandas.read_json(os.path.join(ABSPATH_PROJECT, 'data', 'all_gr_data.json'), orient='index')
+    data_frame.reset_index(inplace=True)
+
+    # Save to CSV
+    data_frame.to_csv(os.path.join(ABSPATH_PROJECT, 'data', 'all_gr_data.csv'), index_label='id', sep=',', encoding='utf-8')
+
